c6088f2e150ec501b00cc39e8273ece4a0a2a4c7
Sean Ashton V. Regalado <seanashton.regalado-23@cpu.edu.ph>
2025-11-30 15:18:33 +0800

    Fix signup redirect issue

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -72,7 +72,7 @@ export default function SignInPage() {
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
           Do not have an account?{' '}
-          <Link href="/auth/signup" className="text-blue-600 hover:underline">
+          <Link href="/signup" className="text-blue-600 hover:underline">
             Sign up
           </Link>
         </p>
